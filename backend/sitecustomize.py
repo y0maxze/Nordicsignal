a5d4fb378b7df830d72371d561d0af4d8c88c5c6
y0maxze <eliashunnes@hotmail.com>
2026-08-26 18:22:29 +0200

    Load official issuer report enrichment

--- backend/sitecustomize.py
+++ backend/sitecustomize.py
@@ -16,6 +16,7 @@ RUNTIME_MODULES = (
     "paper_history_runtime",
     "news_routes",
     "news_cache_limits_runtime",
+    "issuer_reports_runtime",
     "holdings_routes",
     "holdings_tax_runtime",
     "portfolio_instruments_runtime",
