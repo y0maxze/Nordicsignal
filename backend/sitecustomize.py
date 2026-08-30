611dda200610fc02be8d94c446f3986ea7128245
y0maxze <eliashunnes@hotmail.com>
2026-08-30 06:25:47 +0200

    Load Opportunity coverage runtime before autoscan

--- backend/sitecustomize.py
+++ backend/sitecustomize.py
@@ -49,6 +49,7 @@ RUNTIME_MODULES = (
     "opportunity_tracking_runtime",
     "opportunity_performance_v2_runtime",
     "opportunity_calibration_gate_runtime",
+    "opportunity_data_coverage_runtime",
     "opportunity_autoscan_runtime",
     "fund_news_runtime",
     "investment_readiness_runtime",
